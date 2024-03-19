b4227c6a57e069020661ee4eebe99459f3e9fca9
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-19 19:09:14 -0300

    ajuste provider

--- provider.tf
+++ provider.tf
@@ -1,22 +1,6 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.41"
-    }
-  }
 
-
-
-  required_version = ">= 1.3"
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-terraform {
-  required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14.0"
@@ -31,5 +15,6 @@ terraform {
     key    = "api/terraform.tfstate"
     region = "us-east-1"
   }
-  required_version = "~> 1.0"
+
+  required_version = ">= 1.3"
 }
