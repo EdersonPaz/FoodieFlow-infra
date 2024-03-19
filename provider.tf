61050d6a448afb1422166ea015da1de9dfe1ce71
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-19 19:28:52 -0300

    provider

--- provider.tf
+++ provider.tf
@@ -1,6 +1,9 @@
 terraform {
   required_providers {
-
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.41"
+    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14.0"
