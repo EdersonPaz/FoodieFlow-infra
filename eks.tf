4c0161ba03f56c57cc4e5b4b072f9ceadc15ae53
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-19 19:07:17 -0300

    ajustes modelo

--- eks.tf
+++ eks.tf
@@ -1,75 +1,56 @@
-# Definindo a política IAM necessária para o serviço EKS e VPC CNI
-resource "aws_iam_policy" "eks_cni_policy" {
-  name        = "eks-cni-policy"
-  description = "IAM policy for Amazon EKS CNI"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = "ec2:DescribeNetworkInterfaces",
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Associando a política ao papel IAM
-resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
-  role       = aws_iam_role.serviceaccount_role.name
-  policy_arn = aws_iam_policy.eks_cni_policy.arn
-}
-
-# Definindo o cluster EKS
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8"
+  version = "18.29.0"
 
-  cluster_name                             = "cluster-eks-${var.projectName}"
-  cluster_version                          = "1.29"
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
-  subnet_ids                               = module.vpc.public_subnets
-  vpc_id                                   = module.vpc.vpc_id
+  cluster_name    = "cluster-eks-foodieflow"
+  cluster_version = "1.23"
 
-  # Adicionando addons ao cluster EKS
-  cluster_addons = {
-    coredns    = {}
-    kube-proxy = {}
-    vpc-cni    = {}
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 50
   }
 
-  # Definindo o grupo de nós gerenciados EKS
   eks_managed_node_groups = {
-    initial = {
-      instance_types = ["t3.small"]
-
+    general = {
+      desired_size = 2
       min_size     = 1
       max_size     = 5
-      desired_size = 2
 
+      labels = {
+        role = "general"
+      }
 
-      additional_security_group_rules = [
-        {
-          description       = "Allow incoming traffic on port 8080"
-          from_port         = 8080
-          to_port           = 8080
-          protocol          = "tcp"
-          cidr_blocks       = ["0.0.0.0/0"]
-          ipv6_cidr_blocks  = []
-          prefix_list_ids   = []
-          security_group_id = null
-          self              = false
-        }
-      ]
-
-      # Adicionando a política do Amazon EKS CNI
-      additional_policies = [aws_iam_policy.eks_cni_policy.arn]
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
     }
   }
+
+  tags = {
+    Environment = "staging"
+  }
+
+  additional_security_group_rules = [
+    {
+      description       = "Allow incoming traffic on port 8080"
+      from_port         = 8080
+      to_port           = 8080
+      protocol          = "tcp"
+      cidr_blocks       = ["0.0.0.0/0"]
+      ipv6_cidr_blocks  = []
+      prefix_list_ids   = []
+      security_group_id = null
+      self              = false
+    }
+  ]
 }
 
-# Módulo para adicionar addons ao cluster EKS
 module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
   version = "~> 1.16"
@@ -81,10 +62,11 @@ module "eks_blueprints_addons" {
 
   enable_metrics_server = true
 
-  depends_on = [module.eks]
+  depends_on = [
+    module.eks
+  ]
 }
 
-# Recurso Helm para implantar o driver de armazenamento CSI Secrets Store
 resource "helm_release" "csi-secrets-store" {
   name       = "csi-secrets-store"
   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
@@ -100,52 +82,33 @@ resource "helm_release" "csi-secrets-store" {
     value = "true"
   }
 
-  depends_on = [module.eks, module.eks_blueprints_addons]
+  depends_on = [
+    module.eks,
+    module.eks_blueprints_addons
+  ]
 }
 
-# Recurso Helm para implantar o provedor AWS Secrets Store CSI Driver
+
 resource "helm_release" "secrets-provider-aws" {
   name       = "secrets-provider-aws"
   repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
   chart      = "secrets-store-csi-driver-provider-aws"
   namespace  = "kube-system"
 
-  depends_on = [module.eks, module.eks_blueprints_addons, helm_release.csi-secrets-store]
+  depends_on = [
+    module.eks,
+    module.eks_blueprints_addons,
+    helm_release.csi-secrets-store
+  ]
 }
 
-# Recurso Kubernetes para criar um namespace
-resource "kubernetes_namespace_v1" "foodieflownamespace" {
+resource "kubernetes_namespace_v1" "foodieflow_namespace" {
   metadata {
-    name = "foodieflownamespace"
+    name = "foodieflow_namespace"
   }
 
-  depends_on = [module.eks]
+  depends_on = [
+    module.eks
+  ]
 }
 
-# Recurso IAM para definir o papel de serviço
-resource "aws_iam_role" "serviceaccount_role" {
-  name = "aws-iam-serviceaccount-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = { Federated = module.eks.oidc_provider_arn },
-        Action    = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com",
-            "${module.eks.oidc_provider}:sub" = "system:serviceaccount:${var.app_namespace}:${var.serviceaccount_name}"
-          }
-        }
-      }
-    ]
-  })
-}
-
-# Recurso para anexar a política do Amazon EKS CNI ao papel de serviço
-resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
-  role       = aws_iam_role.serviceaccount_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
--- vpc.tf
+++ vpc.tf
@@ -1,39 +1,23 @@
-data "aws_availability_zones" "available" {}
-
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.5.2"
+  version = "3.14.3"
 
-  name                 = "VPC-${var.projectName}"
-  cidr                 = "172.31.0.0/16"
-  azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
-  public_subnets       = ["172.31.80.0/20", "172.31.16.0/20", "172.31.32.0/20"]
+  name = "vpc-foodieflow"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
+  public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
   enable_dns_hostnames = true
   enable_dns_support   = true
-  single_nat_gateway   = true
 
-  # Necessário no Kubernetes
-  public_subnet_tags = {
-    "kubernetes.io/cluster/cluster-eks-${var.projectName}}" = "shared"
-    "kubernetes.io/role/elb"                                = "1"
+  tags = {
+    Environment = "staging"
   }
   map_public_ip_on_launch = true
-}
-
-module "vpc_cni_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 4.12"
-
-  role_name_prefix      = "VPC-CNI-IRSA"
-  attach_vpc_cni_policy = true
-  vpc_cni_enable_ipv6   = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-node"]
-    }
-  }
-
 }
